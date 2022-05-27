df0a9c45f17269b186962db91f50d06b4716c7b7
Aelita-S <1225438527@qq.com>
2022-05-27 18:21:15 +0800

    fix: Invalid distribution name or version syntax

--- bindings/Python/setup.py
+++ bindings/Python/setup.py
@@ -1,6 +1,6 @@
 # coding=utf-8
 from setuptools import setup
 
-setup(name='_judger',
-      version='2.1',
-      packages=["_judger"])
+setup(name='judger',
+      version='2.2',
+      packages=["judger"])
