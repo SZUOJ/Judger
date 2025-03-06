f155a7a17d93cd59ecf62b68de0d653e22e8eb4b
YunhaoDong <dongyunhao2019@email.szu.edu.cn>
2025-03-06 22:41:39 +0800

    Update killer.c

--- src/killer.c
+++ src/killer.c
@@ -1,4 +1,5 @@
 #define _POSIX_SOURCE
+
 #include <pthread.h>
 #include <unistd.h>
 #include <signal.h>
@@ -11,17 +12,17 @@ int kill_pid(pid_t pid) {
 }
 
 
-// time limit should be at least 1000ms
-int get_timeout(int timeout){
-    if (timeout < 1000) return 1000;
-    return timeout;
-}
+//// time limit should be at least 1000ms
+//int get_timeout(int timeout) {
+//    if (timeout < 1000) return 1000;
+//    return timeout;
+//}
 
 
 void *timeout_killer(void *timeout_killer_args) {
     // this is a new thread, kill the process if timeout
-    pid_t pid = ((struct timeout_killer_args *)timeout_killer_args)->pid;
-    int timeout = ((struct timeout_killer_args *)timeout_killer_args)->timeout;
+    pid_t pid = ((struct timeout_killer_args *) timeout_killer_args)->pid;
+    int timeout = ((struct timeout_killer_args *) timeout_killer_args)->timeout;
     // On success, pthread_detach() returns 0; on error, it returns an error number.
     if (pthread_detach(pthread_self()) != 0) {
         kill_pid(pid);
@@ -29,7 +30,7 @@ void *timeout_killer(void *timeout_killer_args) {
     }
     // usleep can't be used, for time args must < 1000ms
     // this may sleep longer that expected, but we will have a check at the end
-    if (sleep((unsigned int)(get_timeout(timeout) / 1000)) != 0) {
+    if (sleep((unsigned int) ((timeout + 1000) / 1000)) != 0) {
         kill_pid(pid);
         return NULL;
     }
